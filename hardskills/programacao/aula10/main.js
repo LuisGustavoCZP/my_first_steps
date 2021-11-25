50d9d55706e3e98b8279de477f00a748affb27d1
LuisGustavoCZP <luisgustavoczp@gmail.com>
2021-11-25 01:49:54 -0300

    aula 10 update 3

--- hardskills/programacao/aula10/main.js
+++ hardskills/programacao/aula10/main.js
@@ -36,8 +36,8 @@ function loadResults (e) {
     let resultLine = window.location.search.replace("?", "");
     let results = resultLine.split("&");
     let resulta = results[0].replace("valuea=", "");
-    let resultb = results[2].replace("valueb=", "");
-    let operator = results[1].replace("operator=", "");
+    let resultb = results[1].replace("valueb=", "");
+    let operator = results[2].replace("operator=", "");
     let valuea = parseInt(resulta);
     let valueb = parseInt(resultb);
     valueaInput.value = valuea;
