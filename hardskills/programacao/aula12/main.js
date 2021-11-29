8dbb0265954f2978c3e501003dc471dfd461a6ae
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2021-11-29 19:54:28 -0300

    Update main.js

--- hardskills/programacao/aula12/main.js
+++ hardskills/programacao/aula12/main.js
@@ -1,6 +1,6 @@
 const frase = "A verdade não está la fora";
 var repeticoes = 100;
-var atual = 0;
+var linhas = 0;
 
 const quadronegro = document.getElementById("quadro-negro");
 const quadroescrita = document.getElementById("quadro-escrita");
@@ -13,30 +13,31 @@ function quadroDif (){
     return quadronegro.clientHeight - escritaHeight();
 }
 
-function EscreverQuadro () {
-    let dif = quadroDif ();
-
-    while (atual < repeticoes && dif >= 0)
-    {
-        quadroescrita.innerText += " " + frase;
-        dif = quadroDif ();
-        atual++;
-    }
-
-    while (dif < 0) {
-        quadroescrita.innerText = quadroescrita.innerText.replace(frase, "");
-        dif = quadroDif ();
-        atual --;
-    }
-
-    return atual < repeticoes;
-}
-
 function LerQuadro () 
 {
     return Math.floor(escritaHeight()/35);
 }
 
+function EscreverQuadro () {
+    let dif = quadroDif ();
+
+    while (repeticoes > 0 && dif >= 0)
+    {
+        quadroescrita.innerText += " " + frase;
+        dif = quadroDif ();
+        repeticoes++;
+    }
+
+    while (dif < 0) {
+        quadroescrita.innerText = quadroescrita.innerText.replace(frase, "");
+        dif = quadroDif ();
+        repeticoes--;
+    }
+    
+    linhas += LerQuadro();
+
+    return repeticoes > 0;
+}
 
 EscreverQuadro ();
 console.log(LerQuadro ());
