3ea1fe201e7b6ac6dd6ebe3150a0c5f4732ace67
LuisGustavoCZP <luisgustavoczp@gmail.com>
2021-11-29 23:03:02 -0300

    enter

--- hardskills/programacao/aula12/main.js
+++ hardskills/programacao/aula12/main.js
@@ -4,7 +4,14 @@ var linhas = 0;
 
 const quadronegro = document.getElementById("quadro-negro");
 const quadroescrita = document.getElementById("quadro-escrita");
+var inputrepeat;
 
+function CriarInputRepeticoes () {
+    inputrepeat = document.createElement("input");
+    inputrepeat.
+}
+
+//#region Sistema A 
 function escritaHeight () {
     return quadroescrita.scrollHeight - 20;
 }
@@ -18,26 +25,46 @@ function LerQuadro ()
     return Math.floor(escritaHeight()/35);
 }
 
-function EscreverQuadro () {
+function EscreverQuadroA () {
     let dif = quadroDif ();
 
     while (repeticoes > 0 && dif >= 0)
     {
         quadroescrita.innerText += " " + frase;
         dif = quadroDif ();
-        repeticoes++;
+        repeticoes--;
     }
 
     while (dif < 0) {
         quadroescrita.innerText = quadroescrita.innerText.replace(frase, "");
         dif = quadroDif ();
-        repeticoes--;
+        repeticoes++;
     }
     
     linhas += LerQuadro();
 
     return repeticoes > 0;
 }
+
+function CreateText (text, type)
+{
+    
+}
+//#endregion
+
+//#region Sistema A
+function EscreverQuadroB () {
+    let dif = 0;
+    while (repeticoes > 0 && dif < 11)
+    {
+        quadroescrita.innerText += " " + frase;
+        dif = quadroDif ();
+        repeticoes--;
+        dif++;
+    }
+    return dif;
+}
+//#endregion
 
 EscreverQuadro ();
 console.log(LerQuadro ());
