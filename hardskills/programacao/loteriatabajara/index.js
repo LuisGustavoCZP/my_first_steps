a1d82c4567f237e83b1bb38f6d46214c46bb87a1
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2021-12-21 01:07:34 -0300

    update loteria

--- hardskills/programacao/loteriatabajara/index.js
+++ hardskills/programacao/loteriatabajara/index.js
@@ -1,25 +1,49 @@
-class Bolinha {
-    constructor(pos, size, sprite)
-    {
+const resultBox = document.getElementById("winners");
 
-    }
+const maxRounds = 6;
+var defaultRow = 
+[
+    01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60
+]
+var row = [];
+var managerID = 0;
+var results = [];
+var round = 0;
+
+function startPlay () {
+    resultBox.innerHTML = "";
+    round = 0;
+    managerID = setInterval(playing, 1000);
+    row = Array.from(defaultRow);//Array.from({length: 60}, (_, i) => i + 1);
 }
 
-function display() {
-    var canvas = document.querySelector("#canvas");
-    var context = canvas.getContext("2d");
-    if (!context) {
-      return;
-    }
+function intText(number) {
+    return (number < 10 ? '0' : '') + number
 }
 
-function main() {
-    var canvas = document.querySelector("#canvas");
-    var context = canvas.getContext("2d");
-    if (!context) {
-      return;
-    }
-
+function drawBall (num) {
+    const ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.innerText = intText(num);
+    return ball;
 }
 
-main();
+function playing () {
+    console.log(row);
+
+    const r = parseInt(Math.random()*row.length);
+    const num = row[r];
+
+    resultBox.append(drawBall(num));
+
+    results.push(num);
+    row.splice(r, 1);
+
+    console.log(results);
+    if(++round >= maxRounds) clearInterval(managerID);
+}
